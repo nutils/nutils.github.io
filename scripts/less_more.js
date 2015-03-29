5f091d17da669d6195c83f3c5fff9bfc47d5a543
Gertjan van Zwieten <git@gjvz.nl>
2015-03-29 22:11:00 +0200

    style updates for news, gallery

--- scripts/less_more.js
+++ scripts/less_more.js
@@ -1,38 +1,11 @@
-gen_onclick = function( style ) {
-  return function() {
-    if ( style.display == "none" ) {
-      style.display = "block";
-      this.innerHTML = "Less";
-    }
-    else {
-      style.display = "none";
-      this.innerHTML = "More‥";
-    }
-  };
-};
-
-window.onload = function() {
-  var more = document.getElementsByClassName( "more" );
-  previous = null;
+less_more = function( item ) {
+  var more = item.getElementsByClassName( "more" );
   for ( var i = 0; i < more.length; i++ ) {
-    more[i].style.display = "none";
-    var newp = document.createElement( "p" );
-    var newa = document.createElement( "a" );
-    newa.setAttribute( "href", "javascript:;" );
-    for ( var m = more[i]; m != previous; m = m.previousSibling ) {
-      if ( m.id ) {
-        newa.setAttribute( "href", "#" + m.id );
-        break;
-      }
+    style = more[i].style;
+    if ( style.display == "block" ) {
+      style.display = "none";
+    } else {
+      style.display = "block";
     }
-    newa.innerHTML = "More‥";
-    newa.onclick = gen_onclick( more[i].style );
-    newp.appendChild( newa );
-    more[i].parentNode.insertBefore( newp, more[i].nextSibling );
-    if ( newa.href == document.location.href ) {
-      newa.onclick();
-    }
-    previous = more[i];
   }
-  console.log( "everything loaded" );
 };
